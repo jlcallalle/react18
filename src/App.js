df13ec90775c45d1bd7cb77575ff3862d993b330
Jorge Callalle <jlcallalle@gmail.com>
2025-02-04 15:57:21 -0500

    template base react

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './react.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Estructura React 18
+        </p>
+        <a
+          className="App-link"
+          href="https://platzi.com/reactjs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Proyect base
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
