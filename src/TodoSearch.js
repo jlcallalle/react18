dce0b3e9ec0411b8f679ef63300a8bd380e95bdf
Jorge Callalle <jlcallalle@gmail.com>
2025-02-10 16:52:43 -0500

    eventos react

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -5,6 +5,12 @@ function TodoSearch() {
     <input
       placeholder="Cortar cebolla"
       className="TodoSearch"
+      onChange={(event) => {
+        console.log('Escribiste en el TodoSearch');
+        console.log(event);
+        console.log(event.target); //valor del input
+        console.log(event.target.value); // obtenemos el valor del input
+      }}
     />
   );
 }
